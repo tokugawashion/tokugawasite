c7a0eb598ea921aa8dc71058fb5e933332514784
tokugawashion <tokugawa1115@gmail.com>
2024-10-21 14:26:15 +0900

    tikin

---
+++ pages/exampletikin.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div class="container">
+    <h1 class="display-1 m-3 "><strong><ins><mark>美味しさNo.１！</mark>KFC風フライドチキン</ins></strong></h1>
+
+    <p class="h2 m-2">KFC人気レシピ1位！数あるKFCレシピ─どれも何か一味足りない気が。試行錯誤の末の主人も絶賛☆再現率の高いレシピです☆</p>
+  </div>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-12 col-md-4 border  p-3 ">
+        <h2 class="display-2"> <u>材料</u></h2>
+        <ul class="list-group">
+          <li class="list-group-item">鶏肉(お好きな部位)　２〜３枚位</li>
+          <li class="list-group-item">☆牛乳　1/2カップ</li>
+          <li class="list-group-item">☆卵　１個</li>
+          <li class="list-group-item">☆にんにくチューブ　２センチ位</li>
+          <li class="list-group-item">★薄力粉　１カップ</li>
+          <li class="list-group-item">★オールスパイス　小さじ１弱</li>
+          <li class="list-group-item">★顆粒コンソメ　大さじ1/2</li>
+          <li class="list-group-item">★塩　大さじ1/2</li>
+          <li class="list-group-item">あれば★パプリカパウダー　小さじ１</li>
+
+        </ul>
+      </div>
+      <div class="col-12 col-md-4 border  p-3 ">
+        <h2 class="display-3">作り方</h2>
+        <ol>
+          <li>☆をボウルに混ぜ合わせておく</li>
+          <li>ボウルに★を全て混ぜ合わせておく。</li>
+          <li>肉の厚みがある部分を切り開き、厚みを一定にする。鶏肉１枚を２～３等分の大きめに切る。</li>
+          <li>１のボウルに鶏肉を浸す。(時間があれば20分以上浸す。)</li>
+          <li>２のボウルに鶏肉を入れて粉類をまんべんなくまぶし、別の容器に並べていく。粉が余るようでしたら、二度付けをお勧めします！</li>
+          <li>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</li>
+          <li>油を160度に温める。(温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</li>
+          <li>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</li>
+          <li>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。</li>
+        </ol>
+      </div>
+      <div class="col-12 col-md-4 border  p-3 ">
+        <h2 class="display-4">コツ・ポイント</h2>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+            <p>揚げる際鍋底での焦げ防止に衣が軽く揚がるまで菜箸等で時々浮かせて下さい</p>
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+            <p>好みで粉類に粗びき黒胡椒小さじ1/2を足しても◎</p>
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+            <p>にんにくチューブ２センチはガーリックパウダー小さじ１に変えて粉類と混ぜても美味しいです。</p>
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+            <p>パプリカパウダーは無くても可</p>
+          </label>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container text-center">
+    <h2>このレシピの生い立ち</h2>
+
+    <p>今まで作ったKFCレシピは、卵液や粉もかなり余るし、何よりも</p>
+    <p>何かひと味足りない……と思って、やっとKFC味にたどり着きました！</p>
+    <p>どの家庭にもあるような材料で☆</p>
+    <p>牛乳と卵液、粉類も必要量のみになっているので無駄が無く経済的です☆</p>
+  </div>
+</template>
+
+<style></style>
